be6dd20289fdea01d18c49aca5a4038d3362c6d5
AndersEben <AndersEben@users.noreply.github.com>
2022-02-16 15:37:26 +0100

    Update

--- AstroGuide/StartActivity.cs
+++ AstroGuide/StartActivity.cs
@@ -30,7 +30,6 @@ namespace AstroGuide
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
